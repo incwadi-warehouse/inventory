767c2c1b409a834476fafd0226d805ddfe61a277
André Baldeweg <kontakt@andrebaldeweg.de>
2019-01-13 15:39:21 +0100

    db mock

--- db.js
+++ db.js
@@ -2,11 +2,9 @@
 
 'use strict'
 
-const sqlite3 = require('sqlite3');
 const express = require('express')
 const app = express()
 const port = 3000
-const database = './db'
 const bodyParser = require('body-parser');
 
 app.use(function (req, res, next) {
@@ -18,141 +16,107 @@ app.use(function (req, res, next) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/book', (req, res) => {
-  let db = new sqlite3.Database(database, (err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-
-  db.serialize(() => {
-    db.all('SELECT * FROM book WHERE id=$id', { $id: req.query.id }, (err, rows) => {
-      if (err) {
-        console.error(err.message);
+function renderEntries(counter, offset) {
+  if (!offset) offset = 0;
+  let entries = [];
+  for (let i = 1; i <= counter; i++) {
+    entries.push(
+      {
+        "added": "1545496807",
+        "author": "Author",
+        "genre": "1",
+        "id": i + parseInt(offset),
+        "price": "1",
+        "stocked": "1",
+        "title": "Title"
       }
+    );
+  }
 
-      res.json(rows[0])
-    });
-  });
+  return entries;
+}
 
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
+app.get('/book', (req, res) => {
+  return res.json(
+    {
+      "added": "1545496807",
+      "author": "Author",
+      "genre": "1",
+      "id": "1",
+      "price": "1",
+      "stocked": "1",
+      "title": "Title"
     }
-  });
+  );
 })
 
 app.get('/find', (req, res) => {
-  let db = new sqlite3.Database(database, (err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-
-  db.serialize(() => {
-    db.all('SELECT * FROM book WHERE author LIKE $term OR title LIKE $term LIMIT 20 OFFSET $offset', {
-      $term: '%' + req.query.term + '%',
-      $offset: req.query.offset
-    }, (err, rows) => {
-      if (err) {
-        console.error(err.message);
-      }
-
-      res.json(rows)
-    });
-  });
-
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
+  return res.json(
+    renderEntries(20, req.query.offset)
+  );
 })
 
 app.post('/create', (req, res) => {
-  let db = new sqlite3.Database(database, (err) => {
-    if (err) {
-      return console.error(err.message);
+  return res.json(
+    {
+      "added": "1545496807",
+      "author": "Author",
+      "genre": "1",
+      "id": "1",
+      "price": "1",
+      "stocked": "1",
+      "title": "Title"
     }
-  });
-
-  db.serialize(() => {
-    let date = Math.round(new Date().getTime() / 1000);
-    db.all('INSERT INTO book(title, author, price, genre, added) VALUES($title, $author, $price, $genre, $added)', {
-      $title: req.body.title,
-      $author: req.body.author,
-      $price: req.body.price,
-      $genre: req.body.genre,
-      $added: date
-    }, (err, rows) => {
-      if (err) {
-        console.error(err.message);
-      }
-
-      res.json(rows)
-    });
-  });
-
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
+  );
 })
 
 app.post('/update', (req, res) => {
-  let db = new sqlite3.Database(database, (err) => {
-    if (err) {
-      return console.error(err.message);
+  return res.json(
+    data = {
+      "added": "1545496807",
+      "author": "Author",
+      "genre": "1",
+      "id": "1",
+      "price": "1",
+      "stocked": "1",
+      "title": "Title"
     }
-  });
-
-  db.serialize(() => {
-    db.all('UPDATE book SET title=$title, author=$author, genre=$genre, price=$price, stocked=$stocked WHERE id=$id', {
-      $id: req.body.id,
-      $title: req.body.title,
-      $author: req.body.author,
-      $genre: req.body.genre,
-      $price: req.body.price,
-      $stocked: req.body.stocked
-    }, (err, rows) => {
-      if (err) {
-        console.error(err.message);
-      }
-
-      res.json(rows)
-    });
-  });
-
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
+  );
 })
 
 app.get('/genres', (req, res) => {
-  let db = new sqlite3.Database(database, (err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-
-  db.serialize(() => {
-    db.all('SELECT * FROM genre', (err, rows) => {
-      if (err) {
-        console.error(err.message);
+  return res.json(
+    [
+      {
+        "id": "1",
+        "name": "Crime"
+      },
+      {
+        "id": "2",
+        "name": "Novel"
+      },
+      {
+        "id": "3",
+        "name": "History"
+      },
+      {
+        "id": "4",
+        "name": "Politics"
+      },
+      {
+        "id": "5",
+        "name": "Languages"
+      },
+      {
+        "id": "6",
+        "name": "Foreign Language Books"
+      },
+      {
+        "id": "7",
+        "name": "Landscape"
       }
-
-      res.json(rows)
-    });
-  });
-
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
+    ]
+  );
 })
 
 app.listen(port, () => console.log('App listening on http://localhost:' + port))
