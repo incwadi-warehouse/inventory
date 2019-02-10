7178fa9d923bac29e4b8de18a60fefbea3abfda6
André Baldeweg <kontakt@andrebaldeweg.de>
2019-02-10 16:20:15 +0100

    fix

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './../dist'),
     publicPath: '/',
-    filename: '/main.[hash:8].min.js'
+    filename: 'main.[hash:8].min.js'
   },
   module: {
     rules: [
