5dbd7f31cebe3084c675926185bc7adc58c7c87a
André Baldeweg <kontakt@andrebaldeweg.de>
2019-02-10 15:56:33 +0100

    fix

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,7 +2,7 @@
   <section class="section section_fixed">
     <h1>Error 404 - Page not found!</h1>
     <p>The page could not be found! Please change references to this site!</p>
-    <router-link :to="{name: 'index'}">Go to homepage</router-link>
+    <router-link :to="{ name: 'index' }">Go to homepage</router-link>
   </section>
 </template>
 
