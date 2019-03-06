d0edcd006484ee0af7b85f340aee16fcda0d767b
André Baldeweg <kontakt@andrebaldeweg.de>
2019-03-06 15:38:44 +0100

    cleared

--- db.js
+++ db.js
@@ -3,9 +3,10 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
 const port = 3000
-const bodyParser = require('body-parser');
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
