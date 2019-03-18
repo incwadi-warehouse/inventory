a5d4fdc96fe7a55d0120c3994bf1b4456e9458a5
André Baldeweg <kontakt@andrebaldeweg.de>
2019-03-18 13:36:25 +0100

    filter stocked and unstocked

---
+++ src/components/Filters.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <section class="section section_default alignRight">
+      <button class="btn btn_secondary" @click="toggleFilter">Filter</button>
+    </section>
+
+    <section class="section section_default" v-if="filter">
+      <form class="form" @submit.prevent="find">
+        <div class="form_group">
+          <div class="form_item">
+            <input type="checkbox" id="stocked" v-model="stocked">
+            <label for="stocked">Stocked</label>
+          </div>
+        </div>
+        <div class="form_group">
+          <div class="form_item">
+            <button class="btn btn_secondary">Find</button>
+          </div>
+        </div>
+      </form>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'filters',
+  data () {
+    return {
+      filter: false,
+      stocked: true
+    }
+  },
+  methods: {
+    toggleFilter: function () {
+      this.filter = !this.filter
+    },
+    find: function () {
+      this.$store.dispatch('search', {
+        term: this.$store.state.searchTerm,
+        limit: 20,
+        offset: this.$store.state.offset,
+        stocked: this.stocked ? 1 : 0
+      })
+    }
+  }
+}
+</script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,18 +1,21 @@
 <template>
   <section>
     <search/>
+    <filters/>
     <list/>
   </section>
 </template>
 
 <script>
 import Search from '../components/Search'
+import Filters from '../components/Filters'
 import List from '../components/List'
 
 export default {
   name: 'index-view',
   components: {
     Search,
+    Filters,
     List
   }
 }
