9f6c4532fbf9cc84a2a7df20324d24049529cf46
André Baldeweg <kontakt@andrebaldeweg.de>
2019-04-10 22:25:06 +0200

    logout

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -9,11 +9,20 @@
     <li class="nav_item">
       <router-link :to="{ name: 'about' }">{{ $t('about') }}</router-link>
     </li>
+    <li class="nav_item">
+      <a href="/logout" @click.prevent="logout">Logout</a>
+    </li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'navigation'
+  name: 'navigation',
+  methods: {
+    logout: function () {
+      localStorage.removeItem('token')
+      this.$router.push({ name: 'login' })
+    }
+  }
 }
 </script>
