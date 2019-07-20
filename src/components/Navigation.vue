cbf3fee8068a966a2f19becc14bfdb051aade0d5
André Baldeweg <kontakt@andrebaldeweg.de>
2019-07-20 17:43:56 +0200

    fix

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="hamburger">
+  <ul class="hamburger" v-if="isAuthenticated">
     <li :class="menu">
       <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25.000001 25.000001" @click="toggleMenu">
         <path d="M0 2h25v4H0zm0 16h25v4H0zm0-8h25v4H0z" class="icon"/>
