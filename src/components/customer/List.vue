fbf1b62eb15f8d3f4dc15ebc5c2d84ca97223781
André Baldeweg <kontakt@andrebaldeweg.de>
2019-07-23 18:07:10 +0200

    styling improved

--- src/components/customer/List.vue
+++ src/components/customer/List.vue
@@ -1,11 +1,23 @@
 <template>
   <section class="section section_fixed">
     <h1>{{ $t('customers') }}</h1>
-    <ul class="list">
-      <li v-for="customer in customers" :key="customer.id">
-        {{customer.name}} <button class="btn btn_link" @click="remove(customer.id)" v-if="isAdmin">{{ $t('remove') }}</button>
-      </li>
-    </ul>
+
+    <div class="card">
+      <div class="card_item" v-for="customer in customers" :key="customer.id">
+        <ul class="card_options" v-if="isAdmin">
+          <li class="card_option">
+            <button class="card_option_btn" :title="$t('remove')" @click="remove(customer.id)">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 50 50">
+                <path d="M11.2 2v8H2v6h46v-6h-7.36V2zm3.68 4h22.08v4H14.88zM7.52 18v30h34.96V18z" class="icon"/>
+              </svg>
+            </button>
+          </li>
+        </ul>
+        <div class="card_text">
+          {{customer.name}}
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -31,3 +43,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.icon {
+  fill: var(--color-fontDark);
+}
+</style>
