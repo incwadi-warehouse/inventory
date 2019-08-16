b092172063ee69d264d867192c9da68f827fed13
André Baldeweg <kontakt@andrebaldeweg.de>
2019-08-16 15:48:35 +0200

    improved

--- src/components/stats/Stats.vue
+++ src/components/stats/Stats.vue
@@ -1,10 +1,18 @@
 <template>
   <section class="container container_m" v-if="stats">
     <ul class="list">
-      <li><b>{{ stats.all }}</b> {{ $t('all_books') }}</li>
-      <li><b>{{ stats.available }}</b> {{ $t('available_books') }}</li>
-      <li><b>{{ stats.sold }}</b> {{ $t('sold_books') }}</li>
-      <li><b>{{ stats.removed }}</b> {{ $t('removed_books') }}</li>
+      <li>
+        <b>{{ stats.all }}</b> {{ $t('all_books') }}
+      </li>
+      <li>
+        <b>{{ stats.available }}</b> {{ $t('available_books') }}
+      </li>
+      <li>
+        <b>{{ stats.sold }}</b> {{ $t('sold_books') }}
+      </li>
+      <li>
+        <b>{{ stats.removed }}</b> {{ $t('removed_books') }}
+      </li>
     </ul>
   </section>
 </template>
