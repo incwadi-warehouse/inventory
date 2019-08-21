a94e2366a1cd3845b71008c2356ac0c61e4df219
André Baldeweg <kontakt@andrebaldeweg.de>
2019-08-21 22:54:44 +0200

    dark mode splitted out

---
+++ src/components/profile/DarkMode.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container container_m">
+      <button class="btn btn_outline" @click="toggleDarkMode">{{ $t('dark_mode') }}</button>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'dark-mode',
+  methods: {
+    toggleDarkMode: function () {
+      if (document.documentElement.hasAttribute('dark')) {
+        document.documentElement.removeAttribute('dark')
+      } else {
+        document.documentElement.setAttribute('dark', true)
+      }
+    }
+  }
+}
+</script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,28 +4,19 @@
       <h1>{{ $t('profile') }}</h1>
     </section>
     <password/>
-    <section class="container container_m">
-      <button class="btn btn_outline" @click="toggleDarkMode">{{ $t('dark_mode') }}</button>
-    </section>
+    <dark-mode/>
   </section>
 </template>
 
 <script>
 import Password from '../components/profile/Password'
+import DarkMode from '../components/profile/DarkMode'
 
 export default {
   name: 'profile-view',
   components: {
-    Password
-  },
-  methods: {
-    toggleDarkMode: function () {
-      if (document.documentElement.hasAttribute('dark')) {
-        document.documentElement.removeAttribute('dark')
-      } else {
-        document.documentElement.setAttribute('dark', true)
-      }
-    }
+    Password,
+    DarkMode
   }
 }
 </script>
