cdc2104f9bba76e03ec7601e7a6f887056434f3e
André Baldeweg <kontakt@andrebaldeweg.de>
2021-03-26 23:29:41 +0100

    delay request

--- src/components/search/Actionbar.vue
+++ src/components/search/Actionbar.vue
@@ -42,6 +42,7 @@
 <script>
 import SearchFilter from './Filter'
 import SearchFilters from './Filters'
+import _debounce from 'lodash/debounce'
 
 export default {
   name: 'search-search',
@@ -52,6 +53,7 @@ export default {
   data() {
     return {
       showFilter: false,
+      changeRequest: null,
     }
   },
   computed: {
@@ -76,9 +78,15 @@ export default {
       this.$store.commit('author/authors', null)
     },
     change() {
-      if (this.term !== null) {
-        this.$store.dispatch('book/find')
+      if (null !== this.changeRequest) {
+        this.changeRequest.cancel()
       }
+      this.changeRequest = _debounce(() => {
+        if (this.term !== null) {
+          this.$store.dispatch('book/find')
+        }
+      }, 500)
+      this.changeRequest()
     },
   },
 }
